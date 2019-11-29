259e46ed20b16c16ddd13ec3bec310832d38571a
Maxime Beugnet <maxime.beugnet@gmail.com>
2019-11-29 01:18:52 +0100

    add delete

--- src/main/java/com/mongodb/quickstart/Delete.java
+++ src/main/java/com/mongodb/quickstart/Delete.java
@@ -4,11 +4,16 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.json.JsonWriterSettings;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.gte;
 
 public class Delete {
 
@@ -19,7 +24,23 @@ public class Delete {
             MongoDatabase sampleTrainingDB = mongoClient.getDatabase("sample_training");
             MongoCollection<Document> gradesCollection = sampleTrainingDB.getCollection("grades");
 
-            System.out.println(gradesCollection.deleteMany(Filters.gte("student_id", 10000)));
+            // delete one document
+            Bson filter = eq("student_id", 10000);
+            DeleteResult result = gradesCollection.deleteOne(filter);
+            System.out.println(result);
+
+            // findOneAndDelete operation
+            filter = eq("student_id", 10002);
+            Document doc = gradesCollection.findOneAndDelete(filter);
+            System.out.println(doc.toJson(JsonWriterSettings.builder().indent(true).build()));
+
+            // delete many documents
+            filter = gte("student_id", 10000);
+            result = gradesCollection.deleteMany(filter);
+            System.out.println(result);
+
+            // delete the entire collection and its metadata (indexes, chunk metadata, etc).
+            gradesCollection.drop();
         }
     }
 }
