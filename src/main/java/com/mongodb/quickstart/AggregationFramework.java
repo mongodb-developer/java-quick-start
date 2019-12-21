1dba3a94d52cdceba2d101c0d23cd32541b0f410
Maxime Beugnet <maxime.beugnet@gmail.com>
2019-12-21 04:32:58 +0100

    add the java aggregation pipeline

---
+++ src/main/java/com/mongodb/quickstart/AggregationFramework.java
@@ -0,0 +1,69 @@
+package com.mongodb.quickstart;
+
+import com.mongodb.client.*;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.json.JsonWriterSettings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static com.mongodb.client.model.Accumulators.push;
+import static com.mongodb.client.model.Accumulators.sum;
+import static com.mongodb.client.model.Aggregates.*;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Projections.*;
+import static com.mongodb.client.model.Sorts.descending;
+
+public class AggregationFramework {
+
+    public static void main(String[] args) {
+        Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
+        String connectionString = System.getProperty("mongodb.uri");
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            MongoDatabase db = mongoClient.getDatabase("sample_training");
+            MongoCollection<Document> zips = db.getCollection("zips");
+            MongoCollection<Document> posts = db.getCollection("posts");
+            threeMostPopulatedCitiesInTexas(zips);
+            threeMostPopularTags(posts);
+        }
+    }
+
+    /**
+     * find the 3 most densely populated cities in Texas.
+     * @param zips sample_training.zips collection from the MongoDB Sample Dataset in MongoDB Atlas.
+     */
+    private static void threeMostPopulatedCitiesInTexas(MongoCollection<Document> zips) {
+        Bson match = match(eq("state", "TX"));
+        Bson group = group("$city", sum("totalPop", "$pop"));
+        Bson project = project(fields(excludeId(), include("totalPop"), computed("city", "$_id")));
+        Bson sort = sort(descending("totalPop"));
+
+        ArrayList<Document> results = zips.aggregate(Arrays.asList(match, group, project, sort, limit(3)))
+                                          .into(new ArrayList<>());
+        System.out.println("==> 3 most densely populated cities in Texas");
+        results.forEach(printDocuments());
+    }
+
+    /**
+     * find the 3 most popular tags and their post titles
+     * @param posts sample_training.posts collection from the MongoDB Sample Dataset in MongoDB Atlas.
+     */
+    private static void threeMostPopularTags(MongoCollection<Document> posts) {
+        Bson unwind = unwind("$tags");
+        Bson group = group("$tags", sum("count", 1L), push("titles", "$title"));
+        Bson sort = sort(descending("count"));
+        Bson project = project(fields(excludeId(), computed("tag", "$_id"), include("count", "titles")));
+
+        ArrayList<Document> results = posts.aggregate(Arrays.asList(unwind, group, sort, limit(3), project)).into(new ArrayList<>());
+        System.out.println("==> 3 most popular tags and their posts titles");
+        results.forEach(printDocuments());
+    }
+
+    private static Consumer<Document> printDocuments() {
+        return doc -> System.out.println(doc.toJson(JsonWriterSettings.builder().indent(true).build()));
+    }
+}
